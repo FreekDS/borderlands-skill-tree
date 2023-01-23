367596d70aa12652c1b203d233923f76b089f1ff
Freek <freek.de.sagher21@gmail.com>
2023-01-23 20:15:52 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { SkillProvider, SkillTreeGroup, SkillTree } from 'beautiful-skill-tree';
+import { motion, harmony, cataclysm } from "./data/data";
+import theme from './data/theme';
+
+
+const data = [
+  {
+    id: 'first-skill',
+    title: 'The root node',
+    tooltip: {
+      description: "The parent node, all of the descendants will be locked until it's selected",
+    },
+    children: [
+      // rinse and repeat; always repeat.
+    ]
+  }
+]
+
 
 function App() {
   return (
@@ -12,6 +30,19 @@ function App() {
           alt="borderlands logo."
         />
       </div>
+      <SkillProvider>
+        <SkillTreeGroup theme={theme}>
+          {() => {
+            return (
+              <React.Fragment>
+                <SkillTree treeId="motion" title="Motion" data={motion} />
+                <SkillTree treeId="harmony" title="Harmony" data={harmony} />
+                <SkillTree treeId="cataclysm" title="Cataclysm" data={cataclysm} />
+              </React.Fragment>
+            )
+          }}
+        </SkillTreeGroup>
+      </SkillProvider>
     </div>
   );
 }
